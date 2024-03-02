ff007e109db164396e77c6a78f96aba74257c487
Sinovuyo Bomvana <sinovuyobomvana2@gmail.com>
2024-03-02 21:19:44 +0200

    Add project files.

---
+++ FormularApp.Api/Controllers/FansController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormularApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FansController : ControllerBase
+    {
+        [HttpGet("GetFans")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("fans");
+        }
+
+    }
+}
---
+++ FormularApp.UnitTests/Systems/Controllers/TestFansController.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using FormularApp.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormularApp.UnitTests.Systems.Controllers
+{
+    public class TestFansController
+    {
+        [Fact]
+        public async Task Get_OnSuccess_ReturnsStatausCode200()
+        {
+            //Arrage
+            var fansController = new FansController();
+
+            //Act
+            var result = (OkObjectResult) await fansController.Get();
+
+            //Assert
+            result.StatusCode.Should().Be(200);
+        }
+    }
+}
